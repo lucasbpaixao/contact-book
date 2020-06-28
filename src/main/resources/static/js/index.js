0cb6a0ea2e69557976fc3bf32e996f67da7c4675
lucasbpaixao <lucasbpaixao@outlook.com>
2020-06-28 04:05:06 -0300

    Change in page layout and structure using materialize framework

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -21,17 +21,21 @@ function listContacts(promise){
 function updateListOnView(contacts){
     const list = document.getElementById('list');
 
-    console.log(template(contacts));
-
-    list.insertAdjacentHTML('beforeend', template(contacts));
+    if(contacts.length == 0){
+        list.insertAdjacentHTML('beforeend', '<h4>Não há contatos registrados no banco de dados!</h4>');
+    }else{
+        list.insertAdjacentHTML('beforeend', template(contacts));
+    }
 }
 
 function template(contacts){
     return `
-    ${contacts.map(contact => {
-    return `<a onclick="viewContact(${contact.id})" class="list-group-item list-group-item-action"> <img src="imgs/pessoa.png" style="height: 40px; width: 40px;" class="mr-2 rounded-circle"> ${contact.name}</a>`
-    }).join('')}
-
+    ${contacts.map(contact =>
+        `
+         <a onclick="viewContact(${contact.id})" class="list-group-item list-group-item-action">
+<img src="imgs/pessoa.png" style="height: 40px; width: 40px;" class="mr-2 rounded-circle"> ${contact.name}
+         </a>`
+    ).join('')}
     `;
 }
 
